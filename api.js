972a4d89c3ee73b7a5679e56c72a610184261b85
Steven Zych <stevezych@gmail.com>
2021-11-11 19:51:39 -0700

    starting files

---
+++ api.js
@@ -0,0 +1,37 @@
+const exercises = {
+    chest: {
+        barbellChestPress: {
+            description: " "
+        }
+    },
+    back: {
+        bentOverRow: {
+            description: " "
+        }
+    },
+    shoulders: {
+        dumbbellFrontRaise: {
+            description: " "
+        }
+    },
+    biceps: {
+        dumbbellCurl: {
+            description: " "
+        }
+    },
+    triceps:{
+        pullDowns: {
+            description: " "
+        }
+    },
+    abs: {
+        crunches: {
+            description: " "
+        }
+    },
+    legs: {
+        squat: {
+            description: " "
+        }
+    }
+};
